f74451a10709fb49e7911006be158b833217747d
kevindev10 <kevindev1@outlook.com>
2025-06-24 13:49:33 +0300

    Basic app done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+
 
 const appName = process.env.APP_NAME
 
